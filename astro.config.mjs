05164c78543e51dfff502753f6876813ce85c009
isaac836leonardo <jairoisaac836@gmail.com>
2024-11-27 15:01:38 -0500

    Actulizacion a componentes de mat

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astronaut.github.io',
-    base: 'MAT',
-  })
+  integrations: [tailwind()],
+  site: 'https://mowecompany.github.io',
+  base: 'MAT',
+});
